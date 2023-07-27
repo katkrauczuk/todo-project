8e70c8844530e6a01727ac94fccc6a447be74b57
Kathlin Krauczuk <kathlinkrauczuk@gmail.com>
2023-07-27 08:35:11 -0300

    primeiro commit

---
+++ Server.js
@@ -0,0 +1,23 @@
+const cors = require('cors')
+const express = require('express')
+const mongoose = require('mongoose')
+
+
+const routes = require('./routes/ToDoRoute')
+
+require('dotenv').config()
+
+const app = express()
+const PORT = process.env.port || 5000
+
+app.use(express.json())
+app.use(cors())
+
+mongoose
+.connect(process.env.MONGODB_URL)
+.then(()=> console.log(`Connected To MongoDB...`))
+.catch((err) => console.log(err))
+
+app.use(routes)
+
+app.listen(PORT, () => console.log(`Listening on: ${PORT}`))
